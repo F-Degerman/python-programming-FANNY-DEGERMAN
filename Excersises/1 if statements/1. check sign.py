35f8b2b04855eaaeef525a5983090f4135c51bb0
Fanny Degerman <fannydegerman@hotmail.com>
2025-09-15 13:07:11 +0200

    nya filer exercise 1

---
+++ Excersises/1 if statements/1. check sign.py
@@ -0,0 +1,12 @@
+import math
+
+print("Det här programmet kollar om ett tal är positivt, negativt eller noll")
+
+number = float(input("skriv ett tal: "))
+
+if number > 0:
+    print("talet är positivt")
+elif number < 0:
+    print("talet är negativt")    
+else:
+    print("talet är noll")  
