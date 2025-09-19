e1ab6d0dc3a42a7f5fa48d2900bd6c7e42b15848
Fanny Degerman <fannydegerman@hotmail.com>
2025-09-19 13:46:00 +0200

    Sparat övningar och lagt till Jupyter.

--- Labs och Explorations/Lab 2/.py
+++
@@ -1,8 +0,0 @@
-import csv
-import math
-
-with open('datapoints.csv', mode='r') as file:
-    reader = csv.reader(file)
-    header = next(reader)  # Skip the header row
-    data = [float(row[0]) for row in reader]
-    print(data)
