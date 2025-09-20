a2dfbd91b0fe83e787101614b5caab0a24216b68
Fanny Degerman <fannydegerman@hotmail.com>
2025-09-20 13:20:16 +0200

    Påbörjar dokumentation i Jupyter

--- Excersises/0 count with python/1.a. 00-count-with-python.py
+++ Excersises/0 count with python/1.a. 00-count-with-python.py
@@ -1,5 +1,5 @@
 import math                         #importerar matematik från biblioteket
-a = 3                               #variabel a
-b = 4                               #variabel b
+a = 3                               
+b = 4                               
 c = math.sqrt(a**2 + b**2)          #roten ur 
-print (f"Hypotenusan är {c}")       #output svar
+print (f"Hypotenusan är {c}")       
--- Excersises/0 count with python/1.b. 00-Count-with-Python.py
+++ Excersises/0 count with python/1.b. 00-Count-with-Python.py
@@ -2,4 +2,5 @@ import math
 c = 7.0
 a = 5.0
 b = math.sqrt(c**2 - a**2)
+# printar ut värdet på b med en decimal
 print(f"kateter b= {b:.1f} ")
